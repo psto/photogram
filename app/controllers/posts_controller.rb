0a7c0d7c1bacbec6be14eb64ac95ca607b1da643
psto <piotrstojan@gmail.com>
2016-12-11 01:01:52 +0100

    Remove duplications form posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all.order(created_at: :desc)
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -14,18 +15,16 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to root_url
+      redirect_to posts_path
     else
       render :new
     end
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to post_url(@post)
     else
@@ -34,15 +33,18 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
 
-    redirect_to posts_path
+    redirect_to root_url
   end
 
   private
 
     def post_params
       params.require(:post).permit(:caption, :image)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
     end
 end
